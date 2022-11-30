1d8f56b1d5ac97648f9df7e8b5eb8997cb3beb91
eles <yryspaeva22@gmail.com>
2022-11-30 14:47:57 +0600

    first commit

---
+++ school/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    grade = models.IntegerField()
+    
